c5555c139b5c0641578f9e36e711125888298aeb
edgreenww <51371812+edgreenww@users.noreply.github.com>
2020-10-25 09:29:22 +0000

    broken trying to set up vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
 
+<template>
+ <!-- App.vue -->
+
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+    <SideBar />
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+    Waking Watch
+
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+      Test Stuff
+      <!-- <MainContent :articles="articles"></MainContent>  -->
+      <!-- <Calendar /> -->
+      <!-- If using vue-router -->
+      <!-- <router-view></router-view> -->
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
+
+
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */ 
+import axios from 'axios' // importing the axios (a HTTP library) to connects the app with the News API
+import MainContent from './components/MainContent.vue' // import the Main Content component
+import SideBar from './components/SideBar.vue' // import the SideMenu component
+import Calendar from './components/Calendar.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+      //  MainContent, // Register the component
+      //  SideBar,
+     //   Calendar
+  },
+
+  data() {
+    return {
+      drawer: false, // false = Vuetify automatically "do the right thing" to show/hide the drawer
+      api_key:'4efdcffd8da34562a15fdcde13de091a', // Your API Key go here
+      articles: [],
+      errors: [] 
+    }
+  },
+  created () {
+    axios.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key)
+      .then(response => {
+        //this.articles = response.data.articles
+        this.articles = response.data.articles
+        console.log('data:')
+        console.log(response.data.articles) // This will give you access to the full object
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  },
+
+
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
+// eslint-disable-next-line
+/* eslint-disable */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  vuetify: new Vuetify(),
+  components: { App },
+  template: '<App/>'
+})
